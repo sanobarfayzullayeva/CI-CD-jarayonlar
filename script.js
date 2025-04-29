3d0ce122adc19fed763656d529ef1114deae4340
sanobarfayzullayeva <sanobarfayzullayeva71@gmail.com>
2025-04-29 11:56:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const toggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+toggle.addEventListener('click', () => {
+  body.classList.toggle('dark');
+
+  body.classList.toggle('dark-mode');
+});
+
